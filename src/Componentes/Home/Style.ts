0c5304151f2805e1cab102a180144e4536a36957
Ramonfvm <ramonfhelipe547@gmail.com>
2024-10-22 03:33:27 -0300

    ajustando os estilos

--- src/Componentes/Home/Style.ts
+++ src/Componentes/Home/Style.ts
@@ -9,22 +9,63 @@ export const DivForm = styled.div`
     height: 100vh;          
     padding: 20px;
 
-    
+   
+    form {
+        background-color:white; 
+        width: 100%;         
+        max-width: 800px;      
+        height: 400px;       
+        border-radius: 16px;    
+        padding: 20px;         
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); 
+        display: flex;         
+        flex-direction: column;
+        align-items: center;    
+    }
+
     div {
         margin-bottom: 15px; 
         width: 100%;          
-        max-width: 300px;     
+        display: flex;        
+        flex-direction: column;
+        align-items: center;  
     }
 
     label {
+        margin:1.6rem;
         margin-bottom: 5px;   
         display: block;        
+        font-weight: bold;     
+        text-align: center;    
+        font-size: 28px;       
+        font-family: 'Arial', sans-serif;
     }
 
     input {
-        width: 100%;          
+        width: 60%;            
         padding: 10px;        
         border: 1px solid #ccc; 
         border-radius: 4px;   
+        font-size: 20px;      
+        text-align: center;    
+    }
+
+    button {
+        padding: 10px;      
+        background-color: #007bff; 
+        color: white;      
+        border: none;       
+        border-radius: 6px; 
+        cursor: pointer;    
+        margin-top: 15px;   
+        display: block;      
+        width: 30%;         
+        text-align: center;
+        font-size: 22px;
+           
+
+        &:hover {
+            background-color: #0056b3; 
+        }
     }
 `;
